3bb9305fb01a238aaeed740aca6319e713bcd63a
tylerburnett011700 <60198415+tylerburnett011700@users.noreply.github.com>
2020-12-04 14:29:47 -0600

    Finished

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
+ReactDOM.render(<App tasks={DATA} />, document.getElementById("root"));
